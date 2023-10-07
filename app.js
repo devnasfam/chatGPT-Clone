7f7fe5ba78f48505852517f03ed69b31f4e30b6a
Dev Nasfam <90545069+devnasfam@users.noreply.github.com>
2023-10-07 06:33:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const api = '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';
 document.addEventListener("DOMContentLoaded", function() {
 let bottom = document.querySelector(".bottom");
    let input = document.querySelector("#txt");
    let sendbtn = document.querySelector(".uil-message");
    let ul = document.querySelector("#list_cont");
-const api = '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';
     bottom.addEventListener("click",()=>{
         input.focus();
     });
@@ -37,8 +37,7 @@ for (var i = 0; i < 3; i++) {
 }
         let li2 = document.createElement("li");
         li2.className="rchat";
-        li2.appendChild(typingAnimationDiv);
-        
+       li2.appendChild(typingAnimationDiv);
         let li = document.createElement("li");
         li.className="schat";
         li.textContent=input.value;
@@ -54,6 +53,8 @@ for (var i = 0; i < 3; i++) {
         .then(data => {
         let i = 0;
 const intervalId = setInterval(() => {
+    try{
+    if (data.choices && data.choices.length > 0 && data.choices[0].message && data.choices[0].message.content) {
     if (i < data.choices[0].message.content.length) {
         li2.textContent += data.choices[0].message.content[i];
         $(".msgs_cont").scrollTop($(".msgs_cont")[0].scrollHeight);
@@ -61,15 +62,29 @@ const intervalId = setInterval(() => {
     } else {
         clearInterval(intervalId);
     }
+    }else{
+        li2.textContent = JSON.stringify(data);
+    }
+    }catch(err){
+        li2.textContent=`Not working because I've reached my monthly API usage quota.
+You can visit this link: https://platform.openai.com/account/api-keys to create your own API key. Replace 'api' variable with the API key you generate in line 1: of my JavaScript code. Thank you! Don't forget to upvote and comment.`;
+        ul.appendChild(li2);
+    }
 }, 20);
+if(data.choices[0].message.content){
 ul.appendChild(li2);
 sendbtn.disabled = false;
 $(".msgs_cont").scrollTop($(".msgs_cont")[0].scrollHeight);
+}
         }).catch(error=>{
-            li2.textContent=error;
+            li2.textContent=`Not working, because I've reached my monthly API usage quota.
+You can visit this link: https://platform.openai.com/account/api-keys to create your own API key. Replace 'api' variable with the API key you generate in line 1: of my JavaScript code. Thank you! Don't forget to upvote and comment.`;
          ul.appendChild(li2);
          $(".msgs_cont").scrollTop($(".msgs_cont")[0].scrollHeight);
         });
    }
    } sendbtn.addEventListener("click",ChatGPT);
 });
+alert(`If the code is showing an error, it's because I've reached my monthly API usage quota.
+You can visit this link: https://platform.openai.com/account/api-keys to create your own API key. Replace 'api' variable with the API key you generate in line 1: of my JavaScript code. Thank you! Don't forget to upvote and comment.`);
+//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
